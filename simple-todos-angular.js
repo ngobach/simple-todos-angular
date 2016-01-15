31bc89afb4d0aa9f2767ce5887f2ae9b4b63b198
ThanBaiKS <thanbaiks@gmail.com>
2016-01-16 00:21:11 +0700

    Step 9

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -2,6 +2,10 @@ Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
  
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
   function onReady() {
     angular.bootstrap(document, ['simple-todos']);
   }
@@ -10,26 +14,50 @@ if (Meteor.isClient) {
     angular.element(document).on('deviceready', onReady);
   else
     angular.element(document).ready(onReady);
-  
+
   // This code only runs on the client
-  angular.module('simple-todos',['angular-meteor']);
+  angular.module('simple-todos',['angular-meteor','accounts.ui']);
  
   angular.module('simple-todos').controller('TodosListCtrl', ['$scope','$meteor',
     function ($scope,$meteor) {
- 
+
+
+      $scope.hideCompleted = false;
+      $scope.$watch('hideCompleted', function (){
+        if ($scope.hideCompleted)
+          $scope.query = {checked: {$ne: true}};
+        else
+          $scope.query = {};
+      });
+      $scope.query = {};
+
       $scope.tasks = $meteor.collection(function (){
-        return Tasks.find({},{sort: {createAt: -1}});
+        return Tasks.find($scope.getReactively('query'),{sort: {createAt: -1}});
       });
       $scope.text = '';
 
       $scope.addTask = function() {
-        if ($scope.text == '')
+        // Check empty text and user login
+        if ($scope.text == '' || Meteor.userId() == null)
           return;
-        $scope.tasks.push({
+        var insert = {
           text: $scope.text,
-          createAt: new Date()
-        });
+          createAt: new Date(),
+          owner: Meteor.userId(),
+          username: Meteor.user().username
+        };
+        $scope.tasks.push(insert);
         $scope.text = '';
       };
+
+      $scope.incompleteCount = function () {
+        return Tasks.find({ checked: {$ne: true} }).count();
+      };
+      $scope.checkEdit = function (e){
+        $scope.edit = true;
+      };
+      $scope.test = function () {
+        console.log(Meteor.user().username);
+      }
   }]);
 }
